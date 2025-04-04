cab7f1899477297b45c8b4274c748068211c287f
Ella <ella6010@gmail.com>
2025-04-04 17:17:10 +0900

    React 상태 끌어올리기 class 실습

--- src/App.jsx
+++ src/App.jsx
@@ -56,24 +56,37 @@ import './App.css';
 // }
 
 class ClassApp extends Component {
-  state = { counter: 1 }
+  state = { counter: 1 };
+
+  incrementCounter = () => {
+    this.setState({counter: this.state.counter + 1})
+  }
+  decrementCounter = () => {
+    this.setState({counter: this.state.counter - 1})
+  }
+
     render(){
       return (
       <>
       <Count counter={this.state.counter} />
-      <button 
-      onClick={() => this.setState ({counter: this.state.counter + 1})}
-      >
-        +
-      </button>
-      <button
-      onClick={() => this.setState ({counter: this.state.counter - 1})}
-      >
-        -
-      </button>
+      <PlusButton incrementCounter={this.incrementCounter} />
+      <MinusButton decrementCounter={this.decrementCounter}/>
       </>
       );
     }
+}
+
+class PlusButton extends Component {
+  render() {
+    return <button onClick={this.props.incrementCounter}>+
+    </button>;
+  }
+}
+class MinusButton extends Component {
+  render() {
+    return <button onClick={this.props.decrementCounter}>-
+    </button>;
+  }
 }
 
 class Count extends Component {
