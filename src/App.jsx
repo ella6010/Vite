c7abbeb21e94d0c28c11efc1e852a0c20b8d645c
Ella <ella6010@gmail.com>
2025-04-04 17:40:42 +0900

    React 상태 끌어올리기 function 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,82 @@
 import { Component, useState } from 'react';
 import './App.css';
 
-// function FunctionApp () {
-//   const [counter, setCounter] = useState(0);
-//   return(
-//   <>
-//   <Count counter={counter} hello={"hello"} array={[1, 2, 3, "안녕하세요"]}/>
-//   <button 
-//     onClick={() => {
-//       setCounter((prev) => prev + 3);
-//       }}
-//     >
-//       +
-//     </button>
-//     <button 
-//     onClick={() => {
-//       setCounter((prev) => prev - 3);
-//       }}
-//     >
-//       -
-//     </button>
-//   </> 
-//   );
+function FunctionApp () {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrementCounter = () => {
+    setCounter(counter - 1)
+  }
+
+  return(
+    <>
+      <Count counter={counter} />
+      <PlusButton setCounter={setCounter} incrementCounter={incrementCounter}/>
+      <MinusButton setCounter={setCounter} decrementCounter={decrementCounter}/>
+    </> 
+  );
+}
+
+function PlusButton({setCounter, incrementCounter}) {
+  return <button onClick={incrementCounter}>+</button>;
+}
+
+function MinusButton({setCounter, decrementCounter}) {
+  return <button onClick={decrementCounter}>-</button>;
+}
+
+function Count({counter}) {
+  return <div>counter : {counter}</div>;
+}
+
+// class ClassApp extends Component {
+//   state = { counter: 1 };
+
+//   incrementCounter = () => {
+//     this.setState({counter: this.state.counter + 1})
+//   }
+//   decrementCounter = () => {
+//     this.setState({counter: this.state.counter - 1})
+//   }
+
+//     render(){
+//       return (
+//       <>
+//       <Count counter={this.state.counter} />
+//       <PlusButton incrementCounter={this.incrementCounter} />
+//       <MinusButton decrementCounter={this.decrementCounter}/>
+//       </>
+//       );
+//     }
 // }
 
-// function Count({array, counter, hello}) {
-//   console.log('array', array);
-//   console.log('counter', counter);
-//   console.log('hello', hello);
-
-//   return <div>counter : {counter}</div>;
+// class PlusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.incrementCounter}>+
+//     </button>;
+//   }
+// }
+// class MinusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.decrementCounter}>-
+//     </button>;
+//   }
 // }
 
-// function App() {
-//   const [counter, setCounter] = useState(0)
-//   return (
-//   <>
-//     <div>counter : {counter}</div>
-//     <button 
-//     onClick={() => {
-//       setCounter((prev) => prev + 1);
-//       }}
-//     >
-//       +
-//     </button>
-//     <button 
-//     onClick={() => {
-//       setCounter((prev) => prev - 1);
-//       }}
-//     >
-//       -
-//     </button>
-//   </>
-//   );
+// class Count extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return <div>counter : {this.props.counter}</div>;
+//   }
 // }
 
-class ClassApp extends Component {
-  state = { counter: 1 };
 
-  incrementCounter = () => {
-    this.setState({counter: this.state.counter + 1})
-  }
-  decrementCounter = () => {
-    this.setState({counter: this.state.counter - 1})
-  }
-
-    render(){
-      return (
-      <>
-      <Count counter={this.state.counter} />
-      <PlusButton incrementCounter={this.incrementCounter} />
-      <MinusButton decrementCounter={this.decrementCounter}/>
-      </>
-      );
-    }
-}
-
-class PlusButton extends Component {
-  render() {
-    return <button onClick={this.props.incrementCounter}>+
-    </button>;
-  }
-}
-class MinusButton extends Component {
-  render() {
-    return <button onClick={this.props.decrementCounter}>-
-    </button>;
-  }
-}
-
-class Count extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <div>counter : {this.props.counter}</div>;
-  }
-}
-
-
-// export default FunctionApp
-export default ClassApp;
+export default FunctionApp
+// export default ClassApp;
